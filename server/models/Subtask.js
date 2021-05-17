30174048b08578d6fb2c0341abacc11a81e510e2
Jason Ngan <jason.ngan@shopee.com>
2021-05-18 01:09:17 +0800

    add timestamp

--- server/models/Subtask.js
+++ server/models/Subtask.js
@@ -1,24 +1,27 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-subtaskSchema = new Schema({
-  order: {
-    type: Number,
-    required: true,
-  },
-  content: {
-    type: String,
-    default: "",
-  },
-  checked: {
-    type: Boolean,
-    default: false,
-  },
-  task_id: {
+subtaskSchema = new Schema(
+  {
+    order: {
+      type: Number,
+      required: true,
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+    task_id: {
       type: Schema.Types.ObjectId,
       ref: "Task",
       required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 export const Subtask = mongoose.model("Subtask", subtaskSchema);
--- server/models/Task.js
+++ server/models/Task.js
@@ -1,35 +1,39 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const taskSchema = new Schema({
+const taskSchema = new Schema(
+  {
     order: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     content: {
-        type: String,
-        default: "",
+      type: String,
+      default: "",
     },
     pomodoro_duration: {
-        type: Number,
-        default: 45,
+      type: Number,
+      default: 45,
     },
     pomodoro_progress: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     checked: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     date: {
-        type: Date,
-        default: Date.now()
+      type: Date,
+      default: Date.now(),
     },
     user_id: {
-        type: Schema.Types.ObjectId, ref: 'User',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-});
+  },
+  { timestamps: true }
+);
 
 export const Task = mongoose.model("Task", taskSchema);
