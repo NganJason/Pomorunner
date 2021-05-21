91f8dc0fbc0d37d4b2700eae762489c16645e970
Jason Ngan <jason.ngan@shopee.com>
2021-05-21 09:48:22 +0800

    temporarily add google login button in FE

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
 import TaskList from "../src/components/TaskList/TaskList.js";
 
+import axios from "axios";
+import GoogleLogin from "react-google-login";
+
+const responseGoogle = async (response) => {
+  console.log(response.getBasicProfile())
+  const res = await axios({
+    method:"POST",
+    url: "http://localhost:5000/api/user/login",
+    data: {tokenID: response.tokenID}
+  })
+
+  console.log(res)
+};
+
 function App() {
   return (
     <div className="App">
-      <TaskList/>
+      <GoogleLogin
+        clientId="491275267203-eej320jk3agt6bf01ii5pfonbi3seu1h.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        isSignedIn={true}
+        cookiePolicy={"single_host_origin"}
+      />
+      <TaskList />
     </div>
   );
 }
