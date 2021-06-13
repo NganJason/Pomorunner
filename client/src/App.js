9de3d56c331c26b65854c3ab9490df82f63b13ba
Jason Ngan <jason.ngan@shopee.com>
2021-06-13 11:22:35 +0800

    revert master change

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import { PersistGate } from "redux-persist/integration/react";
 import React from "react";
 
 import DevAuth from "./auth/components/DevAuth.js";
-import Fade from "@material-ui/core/Fade";
 import ProductionAuth from "./auth/components/ProductionAuth.js";
 import TaskList from "../src/components/TaskList/TaskList.js";
 import SubTaskList from "../src/components/SubTaskList/SubTaskList.js";
@@ -18,35 +17,29 @@ import { store, persistor } from "./redux/store.js";
 dotenv.config();
 
 function App() {
-  const [loading, setLoading] = React.useState(true)
-
   React.useEffect(() => {
     getService();
     console.log("Init services")
   }, []);
 
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Fade>
-          <div className="App">
-            {process.env.NODE_ENV === "development" ? (
-              <DevAuth />
-            ) : (
-              <ProductionAuth />
-            )}
-            <Countdown />
-            <Grid container>
-              <Grid item xs={4}></Grid>
-              <Grid item xs={4}>
-                <TaskList />
-              </Grid>
-              <Grid item xs={4}>
-                <SubTaskList />
-              </Grid>
+        <div className="App">
+        {process.env.NODE_ENV === "development" ? <DevAuth /> : <ProductionAuth />}
+          <Countdown />
+          <Grid container>
+            <Grid item xs={4}>
             </Grid>
-          </div>
-        </Fade>
+            <Grid item xs={4}>
+              <TaskList/>
+            </Grid>
+            <Grid item xs={4}>
+              <SubTaskList/>
+            </Grid>
+          </Grid>
+        </div>
       </PersistGate>
     </Provider>
   );
