8b6751cc1eb3c9949818af3d732ee74e0c0d7a12
Jason Ngan <jason.ngan@shopee.com>
2021-07-04 14:46:18 +0800

    add seed-random

--- client/src/utils/imgUtils.js
+++ client/src/utils/imgUtils.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import seed from "seed-random"
 
 const CLIENT_ID = "wCHnFVRCaC6wmrc6dwVlt89UaCv67yey-UqZPy5atKo";
 const UNSPLASH_LANDSCAPE_COLLECTION_ID = "jhxo2Yp9MQ0";
@@ -7,8 +8,8 @@ const ITEMS_PER_PAGE = 10
 
 const getBackgroundImg = async (width, height) => {
     let dt = new Date();
-    let seed = dt.getDay() + dt.getMonth() + dt.getHours();
-    let generator = new Math.seedrandom(seed);
+    let randomSeed = dt.getDay() + dt.getMonth() + dt.getHours();
+    let generator = new seed(randomSeed);
     let randomNumber = generator();
 
     let randomPage = Math.floor(randomNumber * MAX_PAGE)
